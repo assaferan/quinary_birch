36499f865592147dc7f5ad83ba2821a77e169a23
Eran Assaf <assaferan@gmail.com>
2021-06-08 15:46:58 -0400

    Update birch.cpp

--- src/birch.cpp
+++ src/birch.cpp
@@ -9,8 +9,10 @@ int main()
   Integer<Z64> b = Z64(46);
 
   EuclideanDomain< Integer<Z64> >::XGcdRes d_s_t = a.xgcd(b);
-  
-  std::cout << d_s_t << std::endl;
+
+  for (size_t i = 0; i < 3; i++)
+    std::cout << std::get<i>(d_s_t);
+  std::cout << std::endl;
   
   return 0;
 }
