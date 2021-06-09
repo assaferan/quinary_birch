1db6c7b37e34aeaf7b1be28b6228804693d59cde
Eran Assaf <assaferan@gmail.com>
2021-06-09 08:32:27 -0400

    Update birch_util.cpp

--- src/birch_util.cpp
+++ src/birch_util.cpp
@@ -71,14 +71,6 @@ namespace birch_util
   }
 
   template<>
-  Z convert_Integer<Z128>(const Z128& x)
-  {
-    W128 abs_x = x < 0 ? -x : x;
-    Z ret = convert_Integer<W128, Z>(abs_x);
-    return x < 0 ? -ret : ret;
-  }
-
-  template<>
   Z convert_Integer<W128>(const W128& x)
   {
     W64 top = x >> 64;
@@ -90,6 +82,14 @@ namespace birch_util
     return ret;
   }
 
+  template<>
+  Z convert_Integer<Z128>(const Z128& x)
+  {
+    W128 abs_x = x < 0 ? -x : x;
+    Z ret = convert_Integer<W128, Z>(abs_x);
+    return x < 0 ? -ret : ret;
+  }
+  
   template<>
   Z64 convert_Integer<Z>(const Z& x)
   {
