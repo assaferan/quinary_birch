a1163ec887ff31afb8d5ac7ea65cc3a7eee1c8c2
Eran Assaf <assaferan@gmail.com>
2021-06-09 12:00:13 -0400

    Update FpElement.h

--- src/FpElement.h
+++ src/FpElement.h
@@ -19,37 +19,35 @@ public:
   const std::shared_ptr< const Fp<R, S> > & field() const {return _GF; }
 
   // arithmetic
-  FpElement<R, S> operator+() const {return FpElement(_GF, _val); }
-  FpElement<R, S> operator++(int)
+  FpElement<R,S> operator+() const {return FpElement(_GF, _val); }
+  FpElement<R,S> operator++(int)
   {(this->_val)++; return (*this); }
-  FpElement<R, S> operator-() const;
-  FpElement<R, S> operator+(const FpElement<R, S> &other) const;
-  FpElement<R, S> operator-(const FpElement<R, S> &other) const;
-  FpElement<R, S> operator*(const FpElement<R, S> &other) const;
-  FpElement<R, S> operator/(const FpElement<R, S> &other) const;
-  FpElement<R, S> inverse(void) const;
+  FpElement<R,S> operator-() const override;
+  FpElement<R,S> operator+(const FpElement<R,S> &other) const override;
+  FpElement<R,S> operator-(const FpElement<R,S> &other) const override;
+  FpElement<R,S> operator*(const FpElement<R,S> &other) const override;
+  FpElement<R,S> operator/(const FpElement<R,S> &other) const override;
+  FpElement<R,S> inverse(void) const override;
   
-  FpElement<R, S> & operator+=(const FpElement<R, S> &other);
-  FpElement<R, S> & operator-=(const FpElement<R, S> &other);
-  FpElement<R, S> & operator*=(const FpElement<R, S> &other);
-  FpElement<R, S> & operator/=(const FpElement<R, S> &other);
+  FpElement<R,S> & operator+=(const FpElement<R, S> &other) override;
+  FpElement<R,S> & operator-=(const FpElement<R, S> &other) override;
+  FpElement<R,S> & operator*=(const FpElement<R, S> &other) override;
+  FpElement<R,S> & operator/=(const FpElement<R, S> &other) override;
   
   FpElement<R, S> sqrt() const;
   // assignment and conversion
-  FpElement<R, S> & operator=(const FpElement<R, S> &other);
+  FpElement<R, S> & operator=(const FpElement<R,S> &other) override;
   FpElement<R, S> & operator=(const R &other)
   { this->_val = other; return (*this); }
   
   //boolean
-  bool isZero(void) const
+  bool isZero(void) const override
   {return (this->_val % this->_GF->prime() == 0);}
-  bool operator==(const FpElement<R, S> &other) const {
-    assert( (_GF != 0) && (other._GF != 0) ); 
-    if (this->_GF->prime() != other._GF->prime()) return false;
-    return ((this->_val - other._val) % (this->_GF->prime()) == 0);
-  }
-  bool operator!=(const FpElement<R, S> &other) const
-  { return !((*this)==other); }
+
+  bool isOne(void) const override
+  {return (this->_val % this->_GF->prime() == 1);}
+  
+  bool operator==(const FpElement<R, S> &other) const override;
   
   // This is for sorting, we use the lift for that
   // !! TODO - Is it useful in anyway? shoul we get rid fof that
@@ -67,7 +65,7 @@ public:
   void setField(std::shared_ptr<const Fp<R,S>> fld) {this->_GF = fld;}
   
 protected:
-  std::shared_ptr< const Fp<R, S> > _GF;
+  std::shared_ptr< const Fp<R,S> > _GF;
   R _val;
   
 };
