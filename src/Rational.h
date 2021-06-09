9b6b315894615f511874ab703bfc32579a721383
Eran Assaf <assaferan@gmail.com>
2021-06-09 16:50:05 -0400

    Update Rational.h

--- src/Rational.h
+++ src/Rational.h
@@ -153,7 +153,7 @@ public:
   inline const Rational<R>* getPtr() const { return this; }
 
   inline std::shared_ptr<const RationalField<R> > parent() const override
-  {return std::make_shared<RationalField<R> >(RationalField<R>::getInstance()); }
+  {return RationalField<R>::getInstance().getPtr(); }
   
 protected:
   Integer<R> _num;
