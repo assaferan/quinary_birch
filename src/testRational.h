294e22ba8aa11f7a6cf0b783226dff01093007f2
Eran Assaf <assaferan@gmail.com>
2021-06-09 10:58:28 -0400

    Update testRational.h

--- src/testRational.h
+++ src/testRational.h
@@ -13,21 +13,21 @@ protected:
 
   static void testConstructor(const R & num, const R & denom);
   static void testAdd(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testSub(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testMul(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testDiv(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testPow(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testZero(const R & denom);
   static void testOne(const R & val);
   static void testGcd(const R & num1, const R & denom1,
-		      const R & num2, const R & denom 2);
+		      const R & num2, const R & denom2);
   static void testEuclid(const R & num1, const R & denom1,
-			 const R & num2, const R & denom 2);
+			 const R & num2, const R & denom2);
   
   
 };
