75fdf4ffb7758e966a1ec7fdfb6549bac1cf748e
Eran Assaf <assaferan@gmail.com>
2021-06-10 12:10:07 -0400

    Update QuadFormInt.cpp

--- src/QuadFormInt.cpp
+++ src/QuadFormInt.cpp
@@ -352,9 +352,9 @@ Z_QuadForm<3> Z_QuadForm<3>::getQuadForm(const std::vector<Z_PrimeSymbol>& input
             }
 	  else if (vec[0].isZero())
             {
-	      b += c*vec[2]*vec[2] + f*vec[2];
-	      f += 2*c*vec[2];
-	      h += g*vec[2];
+	      b += (c*vec[2]*vec[2] + f*vec[2]).num();
+	      f += (2*c*vec[2]).num();
+	      h += (g*vec[2]).num();
 
 	      assert( vec[1].isOne() );
 	      assert( b % pp == 0 );
