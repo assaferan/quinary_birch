3d6f7c31ebeeb4e61dd2f87396dcf8e551ebe855
Eran Assaf <assaferan@gmail.com>
2021-06-10 11:30:26 -0400

    Update SquareMatrix.h

--- src/SquareMatrix.h
+++ src/SquareMatrix.h
@@ -21,7 +21,7 @@ public:
   SquareMatrix(const SquareMatrix<R,Parent,n> & mat);
 
   // assignment
-  inline SquareMatrix<R,Parent,n> & operator=(const SquareMatrix<R,Parent,n> &);
+  SquareMatrix<R,Parent,n> & operator=(const SquareMatrix<R,Parent,n> &);
   
   // access
   inline const R& operator()(size_t i, size_t j) const {return mat[i][j]; }
@@ -30,80 +30,80 @@ public:
   inline std::shared_ptr<const Parent> baseRing() const { return _base; };
 
   // return the i-th row
-  inline Vector<R,Parent,n> operator[](size_t i) const;
+  Vector<R,Parent,n> operator[](size_t i) const;
   
   // arithmetic
-  inline SquareMatrix<R,Parent,n> operator*(const SquareMatrix<R,Parent,n>&) const;
-  inline Vector<R,Parent,n> operator*(const Vector<R,Parent,n>& vec) const;
-  inline SquareMatrix<R,Parent,n> operator*(const R &) const;
-  inline SquareMatrix<R,Parent,n> operator/(const R &) const;
+  SquareMatrix<R,Parent,n> operator*(const SquareMatrix<R,Parent,n>&) const;
+  Vector<R,Parent,n> operator*(const Vector<R,Parent,n>& vec) const;
+  SquareMatrix<R,Parent,n> operator*(const R &) const;
+  SquareMatrix<R,Parent,n> operator/(const R &) const;
 
   // booleans
-  inline bool operator==(const SquareMatrix<R,Parent,n>&) const;
+  bool operator==(const SquareMatrix<R,Parent,n>&) const;
   inline bool operator!=(const SquareMatrix<R,Parent,n>& other) const
   { return !((*this) == other);}
   
   // ordering of the matrices for Minkowski reduction
-  inline bool operator<(const SquareMatrix<R,Parent,n>&) const;
+  bool operator<(const SquareMatrix<R,Parent,n>&) const;
   
-  inline bool isUpperTriangular() const;
-  inline bool isLowerTriangular() const;
-  inline bool isSymmetric() const;
-  inline bool isPositiveDefinite() const;
+  bool isUpperTriangular() const;
+  bool isLowerTriangular() const;
+  bool isSymmetric() const;
+  bool isPositiveDefinite() const;
   
   // basic operations
-  inline void setIdentity(void);
+  void setIdentity(void);
   
-  inline SquareMatrix<R,Parent,n> transpose(void) const;
+  SquareMatrix<R,Parent,n> transpose(void) const;
   // !! TODO - save the inverse and track it
   // to save computation
-  inline SquareMatrix<R,Parent,n> inverse(void) const;
-  inline SquareMatrix<R,Parent,n> adjugate(size_t dim) const;
+  SquareMatrix<R,Parent,n> inverse(void) const;
+  SquareMatrix<R,Parent,n> adjugate(size_t dim) const;
   
   template<size_t m>
-  inline SquareMatrix<R,Parent,m> submatrix(size_t idxs[m]) const;
-  inline R determinant(void) const;
+  SquareMatrix<R,Parent,m> submatrix(size_t idxs[m]) const;
+  R determinant(void) const;
 
-  inline Vector<R,Parent,n> solve(const Vector<R,Parent,n> & vec) const;
+  Vector<R,Parent,n> solve(const Vector<R,Parent,n> & vec) const;
 
   // more complex operations that might be useful outside the class
-  inline bool cholesky(SquareMatrix<R,Parent,n>& L, Vector<R,Parent,n> & D) const;
-  inline bool ldl(SquareMatrix<R,Parent,n>& L, Vector<R,Parent,n> & D) const;
+  bool cholesky(SquareMatrix<R,Parent,n>& L, Vector<R,Parent,n> & D) const;
+  bool ldl(SquareMatrix<R,Parent,n>& L, Vector<R,Parent,n> & D) const;
   
   // elementary operations
-  inline void swapRows(size_t row1, size_t row2);
-  inline void swapCols(size_t col1, size_t col2);
-  inline void multiplyRow(size_t row, const R & val);
-  inline void multiplyCol(size_t col, const R & val);
-  inline void addRow(size_t row_to, size_t row_from, const R & val);
-  inline void addCol(size_t col_to, size_t col_from, const R & val);
+  void swapRows(size_t row1, size_t row2);
+  void swapCols(size_t col1, size_t col2);
+  void multiplyRow(size_t row, const R & val);
+  void multiplyCol(size_t col, const R & val);
+  void addRow(size_t row_to, size_t row_from, const R & val);
+  void addCol(size_t col_to, size_t col_from, const R & val);
 
-  inline SquareMatrix<R,Parent,n> hermiteForm(const R & d) const;
+  SquareMatrix<R,Parent,n> hermiteForm(const R & d) const;
   // static functions
   // compute S[idx1]*F*S[idx2]^t
   // this is needed in this form for jordan decomposition
   // F and FParent will be the field of fractions of R, RParent
   template<class F, class FParent>
-  inline static F innerProduct(const SquareMatrix<R,Parent,n> &,
-			       const SquareMatrix<F,FParent,n> &,
-			       size_t, size_t);
+  static F innerProduct(const SquareMatrix<R,Parent,n> &,
+			const SquareMatrix<F,FParent,n> &,
+			size_t, size_t);
   
   // global constants
-  inline static SquareMatrix<R,Parent,n> identity(std::shared_ptr<const Parent>);
+  static SquareMatrix<R,Parent,n> identity(std::shared_ptr<const Parent>);
 
-  inline std::ostream & prettyPrint(std::ostream &, size_t upTo = n) const;
+  std::ostream & prettyPrint(std::ostream &, size_t upTo = n) const;
   
 protected:
   std::shared_ptr<const Parent> _base;
   R mat[n][n];
   
   // helper functions
-  inline void deepCopy(const R mat[n][n]);
+  void deepCopy(const R mat[n][n]);
   
-  inline Vector<R,Parent,n> forwardSubstitution(const Vector<R,Parent,n> & vec) const;
-  inline Vector<R,Parent,n> backwardSubstitution(const Vector<R,Parent,n> & vec) const;
-  inline SquareMatrix<R,Parent,n> inverseLowerTriangular(void) const;
-  inline SquareMatrix<R,Parent,n> inverseUpperTriangular(void) const;
+  Vector<R,Parent,n> forwardSubstitution(const Vector<R,Parent,n> & vec) const;
+  Vector<R,Parent,n> backwardSubstitution(const Vector<R,Parent,n> & vec) const;
+  SquareMatrix<R,Parent,n> inverseLowerTriangular(void) const;
+  SquareMatrix<R,Parent,n> inverseUpperTriangular(void) const;
 };
 
 // left multiplication
@@ -113,7 +113,7 @@ inline SquareMatrix<R,Parent,n> operator*(const R & a, const SquareMatrix<R,Pare
 
 // printing
 template<class R, class Parent, size_t n>
-inline std::ostream& operator<<(std::ostream&, const SquareMatrix<R,Parent,n>&);
+std::ostream& operator<<(std::ostream&, const SquareMatrix<R,Parent,n>&);
 
 namespace std
 {
