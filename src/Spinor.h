9a565aaf16cd942a74f3a03490b70f69dc9cb6d0
Eran Assaf <assaferan@gmail.com>
2021-06-16 13:50:10 -0400

    Update Spinor.h

--- src/Spinor.h
+++ src/Spinor.h
@@ -43,7 +43,9 @@ public:
       std::shared_ptr< SquareMatrixFp<W16,W32,n> > s_p = mod(s.integralMatrix(),GF);
       W16_MatrixFp s_mat(*s_p);
       // we rescale to get the matrix corresponding to s
-      s_mat *= (GF->mod(s.getScale().num()).inverse())^n;
+      W16_FpElement scale(GF, s.getScale().num());
+      W16_FpElement scale_mat = (scale.inverse())^n;
+      s_mat *= scale_mat;
       W16_MatrixFp rad = this->_rads.at(prime);
       W16_FpElement det = s_mat.transpose().restrict(rad).determinant();
       dets.push_back(det);
