1ce0ae69474b804a8244eeef50ba74012a0765b0
Eran Assaf <assaferan@gmail.com>
2021-06-16 13:17:20 -0400

    Update Spinor.h

--- src/Spinor.h
+++ src/Spinor.h
@@ -43,7 +43,7 @@ public:
       std::shared_ptr< SquareMatrixFp<W16,W32,n> > s_p = mod(s.integralMatrix(),GF);
       W16_MatrixFp s_mat(*s_p);
       W16_MatrixFp rad = this->_rads.at(prime);
-      W16_FpElement det = s_mat.restrict(rad).determinant();
+      W16_FpElement det = s_mat.transpose().restrict(rad).determinant();
       dets.push_back(det);
     }
     return this->_computeVals(dets);    
