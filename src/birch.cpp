77c2e90ab41ab2c5d540fd93b277e1ca437dfb06
Eran Assaf <assaferan@gmail.com>
2021-06-17 12:49:34 -0400

    Update birch.cpp

--- src/birch.cpp
+++ src/birch.cpp
@@ -185,9 +185,17 @@ int main()
   std::map<Z64, std::vector< std::vector< NumberFieldElement<Z> > > > evecs = genus.eigenvectors();
 
   std::cout << "eigenvectors = ";
-  for (std::pair<Z64,std::vector< std::vector< NumberFieldElement<Z> > > > evec : evecs) {
-    std::cout << evec.second << std::endl;
+  for (std::pair<Z64,std::vector< std::vector< NumberFieldElement<Z> > > > evecs_k : evecs) {
+    for (std::vector< NumberFieldElement<Z> > evec : evecs_k.second)
+      std::cout << evec << std::endl;
   }
+
+  EigenvectorManager<Z64,3> manager;
+  for (std::pair<Z64,std::vector< std::vector< NumberFieldElement<Z> > > > evecs_k : evecs) {
+    for (std::vector< NumberFieldElement<Z> > evec : evecs_k.second)
+      manager.addEigenvector(evec, evecs_k.first);
+  }
+  manager.finalize();
   
   return 0;
 }
