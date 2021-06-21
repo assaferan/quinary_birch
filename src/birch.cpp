acf0a3589d11b7daab08469c4f0d5b581c6cd49b
Eran Assaf <assaferan@gmail.com>
2021-06-21 08:51:06 -0400

    Update birch.cpp

--- src/birch.cpp
+++ src/birch.cpp
@@ -1,7 +1,21 @@
+#include <sstream>
 #include "TestBirch.h"
 
 int main(int argc, char* argv[])
 {
-  runBirchTests();
+  size_t num_evs = 0;
+  
+  if (argc > 2) {
+    std::cerr << "Usage: " << argv[0] << " [num_evs]" << std::endl;
+    std::cerr << "where [num_evs] is a number specifying how many eigenvalues to test." << std::endl;
+  }
+
+  if (argc == 2) {
+    stringstream num_evs_str(argv[1]);
+    num_evs_str >> num_evs;
+  }
+  
+  runBirchTests(num_evs);
+  
   return 0;
 }
