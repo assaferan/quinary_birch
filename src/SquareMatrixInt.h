9763a08fa812659fe9a295774fe8342b1606ab65
Eran Assaf <assaferan@gmail.com>
2021-06-21 09:52:53 -0400

    Update SquareMatrixInt.h

--- src/SquareMatrixInt.h
+++ src/SquareMatrixInt.h
@@ -24,7 +24,7 @@ public:
   {}
 
   SquareMatrixInt<R,n> operator*(const SquareMatrixInt &) const override;
-  SquareMatrixInt<R,n> transpose(void) const;
+  SquareMatrixInt<R,n> transpose(void) const override;
 };
 
 #include "SquareMatrixInt.inl"
