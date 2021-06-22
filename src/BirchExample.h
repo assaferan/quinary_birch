b5fb26de474697170788667f4f3572f01032f506
Eran Assaf <assaferan@gmail.com>
2021-06-22 17:28:10 -0400

    Update BirchExample.h

--- src/BirchExample.h
+++ src/BirchExample.h
@@ -15,9 +15,12 @@ public:
   BirchExample(const typename QuadFormZZ<R,n>::SymVec &, const R &, size_t,
 	       const std::vector< std::vector< std::vector<R> > > &);
 
+  // n = 3
   static BirchExample<Z64,3> getExample_GV_7_2(void);
-  static BirchExample<Z64,4> getExample_GV_7_3(void);
   static BirchExample<Z64,3> getExample_CMF_49_2_a_a(void);
+  // n = 4
+  static BirchExample<Z64,4> getExample_GV_7_3(void);
+ 
 
   typename QuadFormZZ<R,n>::SymVec coeffs;
   R spinor_prime;
