2549afaccdf145b3dd549b9ba58365c6fafd0e12
Eran Assaf <assaferan@gmail.com>
2021-06-23 08:41:03 -0400

    Update SquareMatrixInt.cpp

--- src/SquareMatrixInt.cpp
+++ src/SquareMatrixInt.cpp
@@ -36,6 +36,21 @@ SquareMatrixInt<Z64,3> SquareMatrixInt<Z64,3>::operator*(const SquareMatrixInt<Z
 }
 
 template<>
+SquareMatrixInt<Z64,5> SquareMatrixInt<Z64,5>::operator*(const SquareMatrixInt<Z64,5>& other) const
+{
+  SquareMatrixInt<Z64,5> prod;
+
+  for (size_t i = 0; i < 5; i++)
+    for (size_t j = 0; j < 5; j++) {
+      prod._mat[i][j] = 0;
+      for (size_t k = 0; k < 5; k++)
+	prod._mat[i][j] += this->_mat[i][k]*other._mat[k][j];
+    }
+  
+  return prod;
+}
+
+template<>
 SquareMatrixInt<Z,2> SquareMatrixInt<Z,2>::operator*(const SquareMatrixInt<Z,2>& other) const
 {
   SquareMatrixInt<Z,2> prod;
