44501409f98ab81edc74d1777327754f9e7a4c10
Eran Assaf <assaferan@gmail.com>
2021-06-28 17:03:08 -0400

    Update factorizations.h

--- src/polyhedral_common/src_number/factorizations.h
+++ src/polyhedral_common/src_number/factorizations.h
@@ -2,13 +2,9 @@
 #define FACTORIZATIONS_INCLUDE
 
 #include <vector>
-
+#include "gmpxx.h"
 
 std::vector<mpz_class> FactorsInt(mpz_class const& x);
-
 std::vector<mpq_class> FactorsInt(mpq_class const& x);
-
-
-
 
 #endif
