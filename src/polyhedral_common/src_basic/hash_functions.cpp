024d84c36c5ff75497f8831635d7d33a63fd7ee7
Eran Assaf <assaferan@gmail.com>
2021-06-28 15:42:52 -0400

    inline function in h file

--- src/polyhedral_common/src_basic/hash_functions.cpp
+++ src/polyhedral_common/src_basic/hash_functions.cpp
@@ -138,14 +138,6 @@ mpz_class MD5_hash_mpz(std::string const& data)
 // Murmurhash function
 // Code from wikipedia
 
-static inline uint32_t murmur_32_scramble(uint32_t k) {
-  k *= 0xcc9e2d51;
-  k = (k << 15) | (k >> 17);
-  k *= 0x1b873593;
-  return k;
-}
-
-
 uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed)
 {
   uint32_t h = seed;
--- src/polyhedral_common/src_basic/hash_functions.h
+++ src/polyhedral_common/src_basic/hash_functions.h
@@ -168,7 +168,13 @@ mpz_class MD5_hash_mpz(std::string const& data);
 // Murmurhash function
 // Code from wikipedia
 
-static inline uint32_t murmur_32_scramble(uint32_t k);
+static inline uint32_t murmur_32_scramble(uint32_t k) {
+  k *= 0xcc9e2d51;
+  k = (k << 15) | (k >> 17);
+  k *= 0x1b873593;
+  return k;
+}
+
 uint32_t murmur3_32(const uint8_t* key, size_t len, uint32_t seed);
 
 namespace std {
