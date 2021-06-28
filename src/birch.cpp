1d006284752eea3777dfe3f7febf54efb5ef9201
Eran Assaf <assaferan@gmail.com>
2021-06-28 13:04:29 -0400

    Update birch.cpp

--- src/birch.cpp
+++ src/birch.cpp
@@ -2,9 +2,7 @@
 
 #include <sstream>
 
-#include "polyhedral_common/src_latt/MatrixCanonicalForm.h"
 #include "TestBirch.h"
-
 
 int main(int argc, char* argv[])
 {
@@ -20,10 +18,6 @@ int main(int argc, char* argv[])
     num_evs_str >> num_evs;
   }
 
-  std::ifstream is("src/polyhedral_common/Examples/ConwaySloane_LatticeNoBasis");
-  MyMatrix<Z32> eMat = ReadMatrix<Z32>(is);
-  Canonic_PosDef<Z32,Z32> RetF = ComputeCanonicalForm<Z32,Z32>(eMat);
-  
   runBirchTests(num_evs);
   
   return 0;
