f3bd5d6d8dd6bada926689d8c8980ddfb0eaf258
Eran Assaf <assaferan@gmail.com>
2021-06-28 16:02:11 -0400

    Update COMB_Stor.h

--- src/polyhedral_common/src_comb/COMB_Stor.h
+++ src/polyhedral_common/src_comb/COMB_Stor.h
@@ -37,84 +37,14 @@ struct IntegerSubsetStorage {
 };
 
 
-void VSLT_ZeroAssignment(IntegerSubsetStorage & VSLT)
-{
-  int MaxElement=VSLT.MaxElement;
-  int Maxp2=MaxElement+2;
-  for (int iVert=0; iVert<Maxp2; iVert++) {
-    VSLT.ListNext[iVert]=-1;
-    VSLT.ListPrev[iVert]=-1;
-  }
-  VSLT.ListNext[MaxElement  ] = MaxElement+1;
-  VSLT.ListPrev[MaxElement+1] = MaxElement;
-}
-
-
-IntegerSubsetStorage VSLT_InitializeStorage(int const& MaxElement)
-{
-  IntegerSubsetStorage VSLT;
-  int Maxp2 = MaxElement+2;
-  std::vector<int> ListNext(Maxp2);
-  std::vector<int> ListPrev(Maxp2);
-  VSLT.ListNext = ListNext;
-  VSLT.ListPrev = ListPrev;
-  VSLT.MaxElement = MaxElement;
-  VSLT_ZeroAssignment(VSLT);
-  return VSLT;
-}
-
-
-int VSLT_NrElement(IntegerSubsetStorage const& VSLT)
-{
-  int MaxElt=VSLT.MaxElement+1;
-  int pos=MaxElt-1;
-  int NbElt=0;
-  while(true) {
-    int posNext=VSLT.ListNext[pos];
-    if (posNext == MaxElt)
-      return NbElt;
-    pos=posNext;
-    NbElt++;
-  }
-}
-
-int VSLT_TheFirstPosition(IntegerSubsetStorage const& VSLT)
-{
-  return VSLT.ListNext[VSLT.MaxElement];
-}
-
-bool VSLT_IsItInSubset(IntegerSubsetStorage const& VSLT, int const& pos)
-{
-  return VSLT.ListNext[pos] != -1;
-}
-
-void VSLT_StoreValue(IntegerSubsetStorage & VSLT, int const& pos)
-{
-  int posAfter=VSLT.ListNext[VSLT.MaxElement];
-  VSLT.ListNext[VSLT.MaxElement] = pos;
-  VSLT.ListNext[pos] = posAfter;
-  VSLT.ListPrev[posAfter] = pos;
-  VSLT.ListPrev[pos] = VSLT.MaxElement;
-}
-
-
-
-void VSLT_RemoveValue(IntegerSubsetStorage & VSLT, int const& pos)
-{
-  int posNext=VSLT.ListNext[pos];
-  int posPrev=VSLT.ListPrev[pos];
-  VSLT.ListNext[posPrev] = posNext;
-  VSLT.ListPrev[posNext] = posPrev;
-  VSLT.ListNext[pos] = -1;
-  VSLT.ListPrev[pos] = -1;
-}
-
-
-bool VSLT_IsEmpty(IntegerSubsetStorage const& VSLT)
-{
-  return VSLT.ListNext[VSLT.MaxElement] == VSLT.MaxElement+1;
-}
-
+void VSLT_ZeroAssignment(IntegerSubsetStorage & VSLT);
+IntegerSubsetStorage VSLT_InitializeStorage(int const& MaxElement);
+int VSLT_NrElement(IntegerSubsetStorage const& VSLT);
+int VSLT_TheFirstPosition(IntegerSubsetStorage const& VSLT);
+bool VSLT_IsItInSubset(IntegerSubsetStorage const& VSLT, int const& pos);
+void VSLT_StoreValue(IntegerSubsetStorage & VSLT, int const& pos);
+void VSLT_RemoveValue(IntegerSubsetStorage & VSLT, int const& pos);
+bool VSLT_IsEmpty(IntegerSubsetStorage const& VSLT);
 
 // This is an artificial class for allowing the operation
 // a[i]=a
@@ -207,39 +137,15 @@ public:
   EncapsDynamicBitset& operator=(const EncapsDynamicBitset&) = delete;
   EncapsDynamicBitset(EncapsDynamicBitset&&) = delete;
   EncapsDynamicBitset() = delete;
-  EncapsDynamicBitset(std::size_t const& eMax) : eVect(eMax)
-  {
-    TotalCount=0;
-  }
-  ~EncapsDynamicBitset()
-  {
-  }
-  std::size_t count() const
-  {
-    return TotalCount;
-  }
-  std::size_t find_first() const
-  {
-    return eVect.find_first();
-  }
-  void set(std::size_t const& pos, bool const& eVal)
-  {
-    if (eVect[pos] && !eVal)
-      TotalCount--;
-    if (!eVect[pos] && eVal)
-      TotalCount++;
-    eVect[pos]=eVal;
-  }
-  bool get(std::size_t const& pos) const
-  {
-    return eVect[pos];
-  }
-  bool empty() const
-  {
-    if (TotalCount == 0)
-      return true;
-    return false;
-  }
+  EncapsDynamicBitset(std::size_t const& eMax);
+  ~EncapsDynamicBitset();
+  
+  std::size_t count() const;
+  std::size_t find_first() const;
+  void set(std::size_t const& pos, bool const& eVal);
+  bool get(std::size_t const& pos) const;
+  bool empty() const;
+  
 private:
   std::size_t TotalCount;
   boost::dynamic_bitset<> eVect;
