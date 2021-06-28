ecdf2e1753560b666fd4ec38e15acdaa3b23a73c
Eran Assaf <assaferan@gmail.com>
2021-06-28 12:59:17 -0400

    Trying again

--- src/birch.cpp
+++ src/birch.cpp
@@ -2,11 +2,7 @@
 
 #include <sstream>
 
-// flint/antic includes
-
-#include "antic/nf.h"
-#include "antic/nf_elem.h"
-
+#include "polyhedral_common/src_latt/MatrixCanonicalForm.h"
 #include "TestBirch.h"
 
 
@@ -24,56 +20,10 @@ int main(int argc, char* argv[])
     num_evs_str >> num_evs;
   }
 
-  nf_t nf;
-  nf_elem_t a,b,c;
-  fmpq_t atrace, btrace, ctrace, ctrace2;
-
-  flint_rand_t state;
-
-  flint_randinit(state);
-
-  nf_init_randtest(nf, state, 25, 200);
+  std::ifstream is("src/polyhedral_common/Examples/ConwaySloane_LatticeNoBasis");
+  MyMatrix<Z32> eMat = ReadMatrix<Z32>(is);
+  Canonic_PosDef<Z32,Z32> RetF = ComputeCanonicalForm<Z32,Z32>(eMat);
   
-  nf_elem_init(a, nf);
-  nf_elem_init(b, nf);
-  nf_elem_init(c, nf);
-
-  fmpq_init(atrace);
-  fmpq_init(btrace);
-  fmpq_init(ctrace);
-  fmpq_init(ctrace2);
-
-  nf_elem_add(c, a, b, nf);
-  nf_elem_trace(atrace, a, nf);
-  nf_elem_trace(btrace, b, nf);
-  nf_elem_trace(ctrace, c, nf);
-  fmpq_add(ctrace2, atrace, btrace);
-
-  assert(fmpq_equal(ctrace, ctrace2));
-
-  fmpq_poly_print_pretty(nf->pol, "x"); std::cerr << std::endl;
-  nf_elem_print_pretty(a, nf, "x"); std::cerr << std::endl;
-  nf_elem_print_pretty(b, nf, "x"); std::cerr << std::endl;
-  nf_elem_print_pretty(c, nf, "x"); std::cerr << std::endl;
-  fmpq_print(atrace); std::cerr << std::endl;
-  fmpq_print(btrace); std::cerr << std::endl;
-  fmpq_print(ctrace); std::cerr << std::endl;
-  fmpq_print(ctrace2); std::cerr << std::endl;
-
-  fmpq_clear(atrace);
-  fmpq_clear(btrace);
-  fmpq_clear(ctrace);
-  fmpq_clear(ctrace2);
-
-  nf_elem_clear(a, nf);
-  nf_elem_clear(b, nf);
-  nf_elem_clear(c, nf);
-
-  nf_clear(nf);
-
-  flint_randclear(state);
-  flint_cleanup();
-
   runBirchTests(num_evs);
   
   return 0;
