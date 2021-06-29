7f02e5a172491fae7b9bfc2f0c4a45b1b3e120db
Eran Assaf <assaferan@gmail.com>
2021-06-29 16:41:27 -0400

    Update POLY_PolytopeFct.h

--- src/polyhedral_common/src_poly/POLY_PolytopeFct.h
+++ src/polyhedral_common/src_poly/POLY_PolytopeFct.h
@@ -424,24 +424,7 @@ Face ComputeFlipping(MyMatrix<T> const& EXT, Face const& OneInc, Face const& sIn
 
 
 
-void PrintListOrbit(std::ostream &os, std::vector<Face> const& ListOrbit)
-{
-  int iOrbit, nbOrbit, siz, i, eVal;
-  nbOrbit=ListOrbit.size();
-  os << "nbOrbit=" << nbOrbit << "\n";
-  for (iOrbit=0; iOrbit<nbOrbit; iOrbit++) {
-    Face eInc=ListOrbit[iOrbit];
-    siz=eInc.count();
-    os << "O" << iOrbit+1 << ": inc=" << siz << "list=";
-    eVal=eInc.find_first();
-    for (i=0; i<siz; i++) {
-      os << " " << eVal;
-      eVal=eInc.find_next(eVal);
-    }
-    os << "\n";
-  }
-}
-
+void PrintListOrbit(std::ostream &os, std::vector<Face> const& ListOrbit);
 
 struct EngelPolyhedralSubordination {
   int n;
